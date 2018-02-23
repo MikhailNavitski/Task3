06b85dd96a8114c9de42efe52135a9814789bf18
OSmol <Olga_Smolyakova@outlook.com>
2018-02-23 15:24:40 +0300

    Update TagType.java

--- Task_3/src/java/by/tc/task3/constant/TagType.java
+++ Task_3/src/java/by/tc/task3/constant/TagType.java
@@ -1,7 +1,7 @@
 package by.tc.task3.constant;
 
 public final class TagType {
-    public final static String OPEN_TYPE = "Открывающий тег";
+    public final static String OPEN_TYPE = "Открывающий тег";/// ЭЭЭЭ, что это? зачем это нужно библиотеке?
     public final static String CLOSE_TYPE = "Закрывающий тег";
     public final static String DECLARATION_TYPE = "Объявление типа документа";
     public final static String ENTITY_TYPE = "Содержимое тега";
