f4cf1164f2ceaa866462a14495308ff70fa4cf22
OSmol <Olga_Smolyakova@outlook.com>
2018-02-23 15:23:46 +0300

    Update ConstantReg.java

--- Task_3/src/java/by/tc/task3/constant/ConstantReg.java
+++ Task_3/src/java/by/tc/task3/constant/ConstantReg.java
@@ -1,8 +1,10 @@
 package by.tc.task3.constant;
 
-public final class ConstantReg {
+public final class ConstantReg {//не жалей символов на имена - Reg - может быть понята как регистрация
     public final static String REG_OPEN = "<[^>]+>";
     public final static String REG_CLOSE = "[a-zA-Z 0-9 _,.+=^&*%$#@!]+>";
     public final static String REG_ENTITY = "([\\S \\n]+</)";
     public final static String REG_META = "<\\?xml version=\"1.0\" encoding=\"UTF-8\"\\?>||<\\?xml version=\"1.0\"\\?>";
+    
+    // конструктор у этого класса д.б. final
 }
